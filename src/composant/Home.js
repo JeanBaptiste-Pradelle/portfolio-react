ec53d9b0a95f4c569444041eaa69a25c6a840494
Jean-Baptiste <76808166+Jeanba63@users.noreply.github.com>
2024-05-16 14:41:35 +0200

    push

--- src/composant/Home.js
+++ src/composant/Home.js
@@ -2,61 +2,58 @@ import React from "react";
 import "../css/home.css";
 
 function Home() {
-  return (
-    <section>
-      <div>
-        <nav className="navbar navbar-expand-lg bg-dark position-fixed top-0 start-0">
-          <div className="container-fluid">
-            <a className="navbar-brand display-1 text-light Dark fs-3" href="#">
-              AC <i className="fa-solid fa-bolt-lightning text-danger"></i>DC
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarScroll"
-              aria-controls="navbarScroll"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-
-            <div className="collapse navbar-collapse" id="navbarScroll">
-              <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll">
-                <li className="nav-item"></li>
-                <li className="nav-item">
-                  <a className="nav-link text-danger" href="#Carousel1">
-                    Accueil
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text-danger" href="#menbreGroupe">
-                    Membres
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text-danger" href="#disco">
-                    Discographie
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text-danger" href="#concert">
-                    Agenda des concerts
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link text-danger" href="#message">
-                    Contact
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </div>
-    </section>
-  );
+	return (
+		<section>
+			<div>
+				<nav className="navbar navbar-expand-lg bg-body-tertiary position-fixed top-0 end-0 m-3">
+					<div className="container-fluid">
+						<a className="navbar-brand" />
+						<button
+							className="navbar-toggler"
+							type="button"
+							data-bs-toggle="collapse"
+							data-bs-target="#navbarSupportedContent"
+							aria-controls="navbarSupportedContent"
+							aria-expanded="false"
+							aria-label="Toggle navigation">
+							<span className="navbar-toggler-icon" />
+						</button>
+						<div
+							className="collapse navbar-collapse"
+							id="navbarSupportedContent">
+							<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+								<li className="nav-item">
+									<a className="nav-link active" aria-current="page" href="#">
+										Home
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link" href="#">
+										A propros
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link" href="#">
+										Réalisation
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link disabled" aria-disabled="true">
+										Temoignage
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link disabled" aria-disabled="true">
+										Contact
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</nav>
+			</div>
+		</section>
+	);
 }
 
 export default Home;
