d9baa7c5e05ae2b9c0d2265e30e79be384a1bf69
Jean-Baptiste <jbpradelle@gmail.com>
2024-05-17 11:07:21 +0200

    react

--- src/App.js
+++ src/App.js
@@ -1,35 +1,46 @@
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import Home from "./composant/Home";
+import { useEffect } from "react";
+import { gsap } from "gsap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import About from "./composant/About";
 import Contact from "./composant/Contact";
 import Temoignage from "./composant/Temoignage";
 import Realisation from "./composant/Realisation";
-
-const element = document.querySelector("main");
-element.addEventListener("wheel", (event) => {
-	event.preventDefault();
-
-	element.scrollBy({
-		left: event.deltaY < 0 ? -30 : 30,
-	});
-});
+import Home from "./composant/Home";
 
 function App() {
-	return (
-		<body>
-			<div className="App">
-				<main>
-					<Home />
-					<About />
-					<Realisation />
-					<Temoignage />
-					<Contact />
-				</main>{" "}
-			</div>{" "}
-		</body>
-	);
+  useEffect(() => {
+    const element = document.querySelector("main");
+
+    const handleWheel = (event) => {
+      event.preventDefault();
+      element.scrollBy({
+        left: event.deltaY < 0 ? -30 : 30,
+      });
+    };
+
+    element.addEventListener('wheel', handleWheel);
+
+    // Clean up function to remove the event listener
+    return () => {
+      element.removeEventListener('wheel', handleWheel);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <main>
+        <Home />
+        <About />
+        <Realisation />
+        <Temoignage />
+        <Contact />
+       
+        
+      </main>
+    </div>
+  );
 }
 
 export default App;
--- src/composant/Temoignage.js
+++ src/composant/Temoignage.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import "../css/Temoignage.css";
 
- function Temoignage() {
+function Temoignage() {
   return (
-    <section className='temoignage'><h1>Temoignage</h1></section>
-  )
+    <section className="temoinSection  w-100 h-100 d-flex flex-column align-items-center justify-content-center justify-self-center">
+      <div className=" w-100 mb-5 d-flex justify-content-center justify-self-center">
+        <h1 className="mx-auto my-2 p-2 w-100 display-5 text-center text-white ">
+          Temoignage
+        </h1>
+      </div>
+      <div className=" temoin d-flex flex-row col-12 ">
+        <div className="col-4 text-white mx-2 my-2 p-2 border border-2 border-black">
+          {" "}
+          <h2>Temoignage 1</h2>
+          <span className="fs-5"> Ceci est le 1er Temoignage d'une longue série</span>
+        </div>
+
+        <div className="col-4 text-white mx-2 my-2 p-2 border border-2 border-black">
+          {" "}
+          <h2>Temoignage 2</h2>
+          <span className="fs-5"> Ceci est le 2eme Temoignage d'une longue série</span>
+        </div>
+
+        <div className="col-4 text-white mx-2 my-2 p-2 border border-2 border-black">
+          {" "}
+          <h2>Temoignage 3</h2>
+          <span className="fs-5"> Ceci est le 3eme Temoignage d'une longue série</span>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Temoignage
+export default Temoignage;
